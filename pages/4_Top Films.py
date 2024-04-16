0cdcb54d0231f44bdfd50595368dbe6e87b35816
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-04-16 14:19:14 +0200

    Update 4_Top Films.py

--- pages/4_Top Films.py
+++ pages/4_Top Films.py
@@ -1,1 +1,6 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.read_csv('https://drive.google.com/file/d/1W60klEPbdt3JV7up4L6_B5YCEFrHcFAb/view?usp=drive_link', sep=','"
+                 
+st.dataframe(df)
