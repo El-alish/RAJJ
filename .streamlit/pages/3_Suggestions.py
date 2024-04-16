47cfd04881ed18547820a79656da706fc8c526fc
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-04-16 10:22:18 +0200

    Create 3_Suggestions.py

---
+++ .streamlit/pages/3_Suggestions.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown("Suggestions de films")
+st.sidebar.markdown("# Suggestion")
+page_title="Suggestions"
+st.title("Venez découvrir des films grâce à notre système de recommandation!")
+st.write("prout")
+page_title="Suggestions"
+st.title("Suggestions de films")
