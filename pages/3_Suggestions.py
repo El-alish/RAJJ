2931d5ee0a240361015271b2d48b08246f7a0ab2
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-04-16 12:25:59 +0200

    Update 3_Suggestions.py

--- pages/3_Suggestions.py
+++ pages/3_Suggestions.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.markdown("Suggestions de films")
-st.sidebar.markdown("# Suggestion")
+st.sidebar.markdown("# Suggestions")
 page_title="Suggestions"
 st.title("Venez découvrir des films grâce à notre système de recommandation!")
 st.write("PAGE EN CONSTRUCTION")
