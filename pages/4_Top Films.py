3046b869822792ea105aeaf0f302523b703d53a5
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-04-16 14:23:02 +0200

    Update 4_Top Films.py

--- pages/4_Top Films.py
+++ pages/4_Top Films.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 df = pd.read_csv('https://drive.google.com/file/d/1W60klEPbdt3JV7up4L6_B5YCEFrHcFAb/view?usp=sharing', sep=',')
                  
-st.dataframe(df)
+st.write(df)
