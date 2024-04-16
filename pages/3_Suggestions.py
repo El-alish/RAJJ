47386e164f79ecf6d1dc1b7b05a96f093f8e401a
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-04-16 10:43:58 +0200

    Update 3_Suggestions.py

--- pages/3_Suggestions.py
+++ pages/3_Suggestions.py
@@ -4,6 +4,6 @@ st.markdown("Suggestions de films")
 st.sidebar.markdown("# Suggestion")
 page_title="Suggestions"
 st.title("Venez découvrir des films grâce à notre système de recommandation!")
-st.write("prout")
+st.write("PAGE EN CONSTRUCTION")
 page_title="Suggestions"
 st.title("Suggestions de films")
