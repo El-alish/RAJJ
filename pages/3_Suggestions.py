4f532c296b97cdf4dc1b1049d031c03a831e565a
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-05-23 11:12:38 +0200

    Delete pages/3_Suggestions.py

--- pages/3_Suggestions.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.markdown("Suggestions de films")
-st.sidebar.markdown("# Suggestions")
-page_title="Suggestions"
-st.title("Venez découvrir des films grâce à notre système de recommandation!")
-st.write("PAGE EN CONSTRUCTION")
-page_title="Suggestions"
-st.title("Suggestions de films")
